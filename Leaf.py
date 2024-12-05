879f624e5c667c19ebdf25fed157c4565447081b
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2024-12-05 15:33:14 -0700

    Update Leaf.py

--- Leaf.py
+++ Leaf.py
@@ -1,18 +1,35 @@
 import pygame
 from pygame.math import Vector2
 import random
+import math
 
 BLUE = (0, 0, 255)
 
 class leaf:
-    def __init__(self):
+    def __init__(self, xpos, ypos):
         self.x = random.randrange(0, 700)
         self.y = random.randrange(0, 700)
         self.vy = 0
         self.vx = 0
+        self.radius = 15
 
     def draw(self, screen):
-        pygame.draw.circle(screen, BLUE, (self.x, self.y), 15)
+        pygame.draw.circle(screen, BLUE, (self.x, self.y), self.radius)
     
-    def move(self, cx, cy):
-        pass
+    #def move(self):
+
+    def collision(self, mousePos, px, py):
+
+        self.x += self.vx
+        self.y += self.vy
+
+        if (self.radius+70 > math.sqrt((px - self.x)**2 + (py - self.y)**2)):
+            print("collision")
+            self.vx, self.vy = 1, 1
+            self.vx = -self.vx*1
+            self.vy = -self.vy*1
+        else:
+            self.vx = 0
+            self.vy = 0
+
+
